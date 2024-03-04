b95ca89cffc77f8e78bbafa17737c52158b03897
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2024-03-04 15:17:29 +0600

    update code

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { useAuth } from "../../API/hooks/useAuth";
 import useAxios from "../../API/hooks/useAxios";
